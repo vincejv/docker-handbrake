6683c42edbaa2894554c61f6a4c159917f416a30
Vince Jerald Villamora <1276544+vincejv@users.noreply.github.com>
2024-04-15 22:24:45 +0800

    clang install fix

--- src/handbrake/build.sh
+++ src/handbrake/build.sh
@@ -218,10 +218,9 @@ apt-get install -y \
 
 # download install clang and llvm
 log "Installing clang and llvm"
-mkdir /tmp/clang
-curl -# -L -f https://github.com/llvm/llvm-project/releases/download/llvmorg-17.0.6/clang+llvm-17.0.6-x86_64-linux-gnu-ubuntu-22.04.tar.xz | tar xJ --strip 1 -C /tmp/clang
-ls -alh /tmp/clang
-export PATH="/tmp/clang/bin:${PATH}"
+wget https://apt.llvm.org/llvm.sh
+chmod +x llvm.sh
+./llvm.sh 18
 
 # install rust, rustup, cargo-c
 log "Installing rust"
