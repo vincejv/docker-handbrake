7be2bf500f70b6389d34790b9b4bfc3e2dbc3939
Vince JV <1276544+vincejv@users.noreply.github.com>
2024-04-19 15:08:48 +0800

    Port windows patches to ubuntu

--- src/handbrake/build.sh
+++ src/handbrake/build.sh
@@ -475,7 +475,9 @@ patch -d /tmp/handbrake -p1 < "$SCRIPT_DIR"/opus_upgrade.patch
 patch -d /tmp/handbrake -p1 < "$SCRIPT_DIR"/language.patch
 patch -d /tmp/handbrake -p1 < "$SCRIPT_DIR"/x264_x265_upgrade.patch
 patch -d /tmp/handbrake -p1 < "$SCRIPT_DIR"/0001-Add-subjective-ssim-in-gui-presets.patch
+patch -d /tmp/handbrake -p1 < "$SCRIPT_DIR"/0001-Allow-the-use-of-extended-CRF.patch
 patch -d /tmp/handbrake -p1 < "$SCRIPT_DIR"/0002-Merge-global-motion-estimation-patch.patch
+patch -d /tmp/handbrake -p1 < "$SCRIPT_DIR"/0001-Fix-CRF-greater-63.patch
 
 # Create the meson cross compile config file.
 # if xx-info is-cross; then
